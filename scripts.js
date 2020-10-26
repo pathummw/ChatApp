53e218e23d1a81e70253733c0f800c51e8664227
Pathum <pathummadushanka.weerathunga@student.kyh.se>
2020-10-26 11:07:46 +0100

    Initia stage of edit messages

--- scripts.js
+++ scripts.js
@@ -1,57 +1,82 @@
 let objectList = [];
+let editIcon;
+let newMessage;
+let edit = false;
+let today;
 
+document.getElementById("btn").addEventListener("click", submit);
 
-
+ 
 function submit(){
 
-        let today = new Date();
+    if(!edit){
+        
+        today = new Date();
         let singleObject = {};
         let name = document.querySelector(".name").value;  //assign textbox values
         let message = document.querySelector(".message").value;
 
-    if(validation(name,message)){
+        if(validation(name,message)){
+            
+            document.querySelector(".name").value = '';  /* clear the input field values */
+            document.querySelector(".message").value = '';
+            singleObject['id'] = objectList.length; 
+            singleObject['name'] = name;
+            singleObject['message'] = message;
+            singleObject['time'] = today;
+            objectList.push(singleObject);
+        
+            let para = document.createElement("div");   //make the div element dynamically
+            para.style.display = 'inline-block';  //style the p element
+            para.style.backgroundColor = 'rgba(31, 145, 238, 0.726)';
+            para.style.borderRadius = '1em';
+            para.style.marginBottom = '0.5em';
+            para.style.padding = '1em';
+            para.style.position = "relative";
+            
+            editIcon = document.createElement("i");
+            editIcon.className = "editIcon fa fa-pencil-square";
+            editIcon.addEventListener("click", function(){editMessage(event,this)},true);
+           
 
-        document.querySelector(".name").value = '';  /* clear the input field values */
-        document.querySelector(".message").value = '';
-        singleObject['name'] = name;
-        singleObject['message'] = message;
-        singleObject['time'] = today;
-        
-        objectList.push(singleObject);
-    
-    
-        let para = document.createElement("p");   //make the p element dynamically
-        para.style.display = 'inline-block';  //style the p element
-        para.style.backgroundColor = 'rgba(31, 145, 238, 0.726)';
-        para.style.borderRadius = '1em';
-        para.style.marginBottom = '0.5em';
-        para.style.padding = '1em';
-        
-        
-        let node;
-        let nodeMsg;
-        let nodeTime;
+            let nodeName;
+            let nodeMsg;
+            let nodeTime;
+            let i = objectList.length-1;
 
-        for(let i=0; i<objectList.length; i++){
-            node = document.createTextNode(objectList[i].name);
+            nodeName = document.createTextNode(objectList[i].name);
             nodeTime = document.createTextNode(objectList[i].time.toLocaleTimeString());
             linebreak = document.createElement("br");
-            nodeMsg = document.createTextNode(objectList[i].message); 
-            para.id = 'id'+[i];  //Assign a id to p
-        }
-        
-        para.appendChild(node);
-        para.appendChild(nodeTime);
-        para.appendChild(linebreak);
-        para.appendChild(nodeMsg);
-        
- 
-        let element = document.getElementById("rightCanvas");
-        element.appendChild(para);
+            nodeMsg = document.createTextNode(objectList[i].message);
+            para.id = [i];  //Assign a id to div 
+                
+                
 
-    } 
-  
-    uppdateTime();
+            
+            para.appendChild(nodeName);
+            para.appendChild(nodeTime);
+            para.appendChild(linebreak);
+            para.appendChild(nodeMsg);
+            para.appendChild(editIcon);
+    
+            let element = document.getElementById("rightCanvas");
+            element.appendChild(para);
+
+    }
+
+        
+
+    }else{
+        alert("Edit");
+        edit = false;
+        submitEditedMessage();
+
+    }
+
+    
+
+        
+    /* uppdateTime(); */
 }
 
 function validation(name,message){
@@ -113,4 +138,106 @@ function uppdateTime(){
     
     let t = setTimeout(uppdateTime, 60000);  //recall the uppdateTime() function every 60seconds to reset p elements time //just now etc.. 60000ms = 60seconds
 }
-/* https://stackoverflow.com/questions/5868850/creating-list-of-objects-in-javascript */
+
+
+function editMessage(event){
+    alert("edit msg");
+
+    edit = true;
+
+    /* edit = true; */  // set the edit true to know if it is edit message or newly created message
+    /* alert("inside edit"); */
+    para = event.target.parentNode;   //get the div that clicked 
+
+    /* console.log(para.id); */ ////////////////////
+
+   /* para.id === objectList[element].id; */
+    /* console.log(para.pId); */
+
+/* if(objectList.id.value === para.id){
+    console.log(objectList.id.value);
+} */
+    let foundObject = objectList.find(obj => parseInt(obj.id) == parseInt(para.id));
+    console.log(foundObject);
+
+    if(foundObject){
+        console.log("found obj");
+        document.querySelector(".name").value = foundObject.name;
+        document.querySelector(".message").value = foundObject.message;
+        /* document.querySelector(".time").value = foundObject.time; */
+    }else{
+        console.log("not found obj");
+    }
+
+
+    if(updateObjectList()){
+        /* document.getElementById("btn").addEventListener("click", submit); */
+        /* alert("Set submit event again"); */
+    }
+
+
+}
+
+function updateObjectList(){
+    /* alert("UpdateObject funt"); */
+
+    //after adding edit the msg, add submit eventlistner again to submit button 
+    
+
+    /* document.querySelector(".name").value = '';  */ /* clear the input field values */
+    /* document.querySelector(".message").value = ''; */
+
+    //update the objectList with new values
+}
+
+function submitEditedMessage(){
+    /* document.getElementById(para.id).innerHTML = `${document.querySelector(".name").value} ${document.querySelector(".message").value} ${today}`; */
+    /* document.getElementById(para.id).innerHTML.name = document.querySelector(".name").value; */
+    /* console.log(document.getElementById(para.id).nodeMsg); */
+
+    /* let textNode = document.getElementById(para.id).firstChild; */
+    /* let childNodes = document.getElementById(para.id).childNodes; */
+    /* console.log(childNodes); */
+    /* console.log(document.querySelector(".name").value); */
+    //update the edited objects in array
+    alert("submitEditedMessage");
+
+
+    document.getElementById(para.id).childNodes[0].nodeValue = document.querySelector(".name").value;
+    document.getElementById(para.id).childNodes[1].nodeValue = today.toLocaleTimeString();
+    document.getElementById(para.id).childNodes[3].nodeValue = document.querySelector(".message").value;
+
+
+
+
+//update objectList with newly edited values
+    objectList[para.id].name = document.querySelector(".name").value;
+    objectList[para.id].message = document.querySelector(".message").value;
+    objectList[para.id].time = today;
+
+    document.querySelector(".name").value = "";
+    document.querySelector(".message").value = "";
+
+    /* edit = false; */
+}
+
+
+/* objectList[0].name */
+
+
+
+/* document.getElementById(1).childNodes[1].nodeValue
+"21:52:54"
+document.getElementById(1).childNodes[0].nodeValue
+"Himansa"
+document.getElementById(1).childNodes[2].nodeValue
+null
+document.getElementById(1).childNodes[3].nodeValue
+
+"22" 
+
+//i icon
+document.getElementById(2).lastChild    
+
+
+*/
